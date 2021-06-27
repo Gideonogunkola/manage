2ab50e8043058bcc39f4e4ed6c5b57b63ef5ac55
Gideon Ogunkola <ogunkolagideon@gmail.com>
2021-06-27 09:36:12 +0100

    copyright text added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
   <div class="bg-sec-veryDarkBlue py-20">
     <div
       class="
+        text-sec-grayishBlue
         max-w-screen-xl
         w-11/12
         m-auto
@@ -10,7 +11,6 @@
         gap-y-14
         leading-9
         tracking-wide
-        text-white
       "
     >
       <div
@@ -23,11 +23,17 @@
           justify-center
         "
       >
-        <img
-          class="pt-3 m-auto w-3/5 lg:w-full mt-14 lg:mt-0"
-          src="@/assets/images/logo-2.svg"
-          alt=""
-        />
+        <div>
+          <img
+            class="pt-3 m-auto w-3/5 lg:w-full mt-14 lg:mt-0"
+            src="@/assets/images/logo-2.svg"
+            alt=""
+          />
+          <p class="mt-16 text-center lg:hidden">
+            Copyright 2020. All Rights Reserved
+          </p>
+        </div>
+
         <div
           class="
             w-4/5
@@ -65,12 +71,29 @@
       <div class="col-span-2 order-1 lg:order-3 lg:justify-self-end">
         <div class="flex">
           <input
-            class="rounded-full pl-4 lg:pl-8 lg:pr-4"
+            class="
+              rounded-full
+              pl-4
+              lg:pl-8 lg:pr-4
+              focus:outline-none
+              focus:ring-2
+              focus:ring-pry-brightRed
+              focus:border-transparent
+            "
             type="text"
             placeholder="Updates in your inbox…"
           />
           <input
-            class="px-9 py-2 lg:py-4 rounded-full ml-2 lg:ml-6 bg-pry-brightRed"
+            class="
+              px-9
+              py-2
+              lg:py-4
+              rounded-full
+              ml-2
+              lg:ml-6
+              bg-pry-brightRed
+              text-white
+            "
             type="button"
             value="Go"
           />
